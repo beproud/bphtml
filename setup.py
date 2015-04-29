250b780831b841496b8dd40bef3e87ee767406d3
Atsushi Odagiri <aodagx@gmail.com>
2015-04-29 15:25:05 +0900

    added porting beproud.utils.html from bputils

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 
+requires = [
+    "BeautifulSoup4",
+    "six",
+    "webhelpers2",
+]
 
 setup(name="bphtml",
       namespace_packages=["beproud"],
       packages=["beproud", "beproud.html"],
+      test_suite="beproud.html",
+      install_requires=requires,
 )
